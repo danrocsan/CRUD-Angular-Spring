48643819180c16aa0a006a4b20ea2fd9cd475126
danrocsan <danrocsan@gmail.com>
2024-05-10 09:49:36 -0300

    Conectando angular com api spring

--- crud-spring/src/main/java/com/daniel/model/Course.java
+++ crud-spring/src/main/java/com/daniel/model/Course.java
@@ -1,5 +1,7 @@
 package com.daniel.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import lombok.Data;
 @Entity
 public class Course {
     @Id
+    @JsonProperty("_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
